9c78718cf5d89ab4e2e99175dea230d74ce367b7
givethedog <mung4717@gmail.com>
2022-01-26 14:39:16 +0000

    https://wikidocs.net/110745 여기까지

--- web_fe/urls.py
+++ web_fe/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from . import views
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    path('', include('common.urls')),
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    # path('', views.index, name='index')
 ]
