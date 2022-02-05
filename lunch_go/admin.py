124673d452ed86ffff4fd24fffc2b9cf264f8389
GiveTheDog <givethedog@gmail.com>
2022-02-06 02:01:35 +0900

    db 추가

--- lunch_go/admin.py
+++ lunch_go/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from lunch_go.models import lunch_go
+
 # Register your models here.
+
+admin.site.register(lunch_go)
--- lunch_go/models.py
+++ lunch_go/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class lunch_go(models.Model):
+    title = models.CharField('팀이름', max_length=200)
+    description = models.TextField('내용')
+    type = models.CharField('점심종류', max_length=100)
+    image = models.ImageField('식당 이미지', blank=True, null=True, upload_to='project')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+# https://zodlab.tistory.com/111?category=699316  << 여기 하던 중
